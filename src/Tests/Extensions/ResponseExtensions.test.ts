0e67ea93043c5ecb25096561c8b6a55ccad07e77
Marko Coetzee <markoc@pbsa.co.za>
2020-03-05 15:58:24 +0200

    Continued writing Tests

--- src/Tests/Extensions/ResponseExtensions.test.ts
+++ src/Tests/Extensions/ResponseExtensions.test.ts
@@ -1,5 +1,19 @@
 import { expect } from "chai";
+import { ResponseExtensions } from "../../Extensions/ResponseExtensions";
 
 describe("ResponseExtensions", () => {
-    describe("")
+    describe("Setup200ResponseCodes", () => {
+
+        let response;
+        before(() => {
+            let responseExtentions = new ResponseExtensions();
+            responseExtentions.Init();
+            response = new Response();
+        })
+
+        it('OK sets status to 200', () => {
+            response.OK();
+            expect(response.status).to.equal(200);
+        })
+    })
 });
